49ecf78f2e3bf56d8101c7df1b0d60b0eeb82f2d
Adriano Avelino <adriano-st777@hotmail.com>
2025-07-06 16:44:55 -0300

    adiciona verificação de envio de email e chamada em sistema externo

--- src/test/java/br/com/deveficiente/mercadolivre/compras/RetornoPagamentoControllerTest.java
+++ src/test/java/br/com/deveficiente/mercadolivre/compras/RetornoPagamentoControllerTest.java
@@ -2,6 +2,7 @@ package br.com.deveficiente.mercadolivre.compras;
 
 import br.com.deveficiente.mercadolivre.compartilhado.seguranca.AutorizacaoHelper;
 import br.com.deveficiente.mercadolivre.compartilhado.seguranca.TokenManager;
+import br.com.deveficiente.mercadolivre.compartilhado.service.Emails;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import net.jqwik.api.Arbitraries;
 import net.jqwik.api.Arbitrary;
@@ -12,6 +13,7 @@ import net.jqwik.api.Provide;
 import net.jqwik.api.constraints.IntRange;
 import net.jqwik.spring.JqwikSpringSupport;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
@@ -28,6 +30,11 @@ import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -47,6 +54,15 @@ class RetornoPagamentoControllerTest {
 
     @MockitoBean
     private RestTemplate restTemplate;
+
+    @MockitoBean
+    private Emails emails;
+
+    @Value("${sistema.externo.notas-fiscais}")
+    private String urlSistemaExternoNotasFiscais;
+
+    @Value("${sistema.externo.rankings}")
+    private String urlSistemaExternoRankings;
 
     @Property(tries = 10)
     @Label("Cadastro de retorno de compra com sucesso")
@@ -82,9 +98,12 @@ class RetornoPagamentoControllerTest {
                 .headers(new AutorizacaoHelper().getAuthorization(tokenManager))
                 .content(objectMapper.writeValueAsString(payloadRetorno)))
                 .andExpect(status().isOk());
+
+        verify(restTemplate, times(1)).postForEntity(eq(urlSistemaExternoNotasFiscais), any(NotaFiscalRequest.class), eq(Void.class));
+        verify(restTemplate, times(1)).postForEntity(eq(urlSistemaExternoRankings), any(RankingRequest.class), eq(Void.class));
     }
 
-    @Property(tries = 10)
+    @Property(tries = 1)
     @Label("Cadastrar retorno compra com erro")
     void cadastrarRetornoCompraComErro(
             @ForAll @IntRange(min = 1, max = 1000) int quantidade,
@@ -117,6 +136,8 @@ class RetornoPagamentoControllerTest {
                         .headers(new AutorizacaoHelper().getAuthorization(tokenManager))
                         .content(objectMapper.writeValueAsString(payloadRetorno)))
                 .andExpect(status().isOk());
+
+        verify(emails, times(1)).enviarEmailFalhaPagamentoCliente(any(Compra.class), anyString());
     }
 
     @Property(tries = 10)
@@ -164,6 +185,9 @@ class RetornoPagamentoControllerTest {
                 .headers(new AutorizacaoHelper().getAuthorization(tokenManager))
                 .content(objectMapper.writeValueAsString(payloadRetornoStatusSucesso)))
                 .andExpect(status().isOk());
+
+        verify(restTemplate, times(1)).postForEntity(eq(urlSistemaExternoNotasFiscais), any(NotaFiscalRequest.class), eq(Void.class));
+        verify(restTemplate, times(1)).postForEntity(eq(urlSistemaExternoRankings), any(RankingRequest.class), eq(Void.class));
     }
 
     public static String extrairIdUrl(String url) {
